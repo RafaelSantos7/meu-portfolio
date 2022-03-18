15c055821e89d668a6c255218a5f1b41a7367676
Rafael <rafaelsantosgoncalves5@gmail.com>
2022-03-18 13:05:00 -0300

    add meu portfolio

---
+++ js.js
@@ -0,0 +1,11 @@
+function mudaTema() {
+    document.body.classList.toggle("dark");
+}
+
+function mudaPagina() {
+    window.location = "index2.html";
+}
+
+function voltar() {
+    window.location = "index.html";
+}
